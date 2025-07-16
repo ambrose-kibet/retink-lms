c5a06a11d14601f8ede2c2a1200774341aff9d01
ambrose-kibet <ambrosekibet36@gmail.com>
2025-07-16 08:34:46 +0300

    Add user prefetch query to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,17 @@ import {
 } from "@tanstack/react-query";
 import { getCourses } from "@/actions/courses";
 import Courses from "@/components/courses/course";
+import { getCurrentUser } from "@/actions/auth";
 
 export default async function Home() {
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
     queryKey: ["courses"],
     queryFn: () => getCourses(),
+  });
+  await queryClient.prefetchQuery({
+    queryKey: ["user"],
+    queryFn: () => getCurrentUser(),
   });
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
