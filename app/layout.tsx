af005907065b57ce569f84516c5bfed9a80cffce
ambrose-kibet <ambrosekibet36@gmail.com>
2025-07-16 08:34:41 +0300

    Remove unnecessary blank line in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import Navbar from "@/components/Navbar";
-
 import { getUserFromCookie } from "@/lib/getuser";
 
 const geistSans = Geist({
